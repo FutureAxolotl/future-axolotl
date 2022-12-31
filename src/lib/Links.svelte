cd97693625720ab62c04d61d427f4f5601447cbf
FutureAxolotl <100148624+FutureAxolotl@users.noreply.github.com>
2022-12-31 11:45:45 -0500

    Add missing end tags

--- src/lib/Links.svelte
+++ src/lib/Links.svelte
@@ -3,8 +3,8 @@
 <div class="container">
   <a href="https://www.youtube.com/@future_axolotl2058"><button><i class="fa-brands fa-youtube" aria-hidden="true" style="font-size:48px;"></i>
 </button></a>
-  <a href="https://github.com/FutureAxolotl"><button><i class="fa-brands fa-github" aria-hidden="true" style="font-size:48px;"></button></a>
-  <a href="https://discord.gg/6HcD2k4W98"><button><i class="fa-brands fa-discord" aria-hidden="true" style="font-size:48px;"></button></a>
+  <a href="https://github.com/FutureAxolotl"><button><i class="fa-brands fa-github" aria-hidden="true" style="font-size:48px;"></i></button></a>
+  <a href="https://discord.gg/6HcD2k4W98"><button><i class="fa-brands fa-discord" aria-hidden="true" style="font-size:48px;"></i></button></a>
 </div>
 
 <style>
